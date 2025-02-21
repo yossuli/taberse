e98086e1a1be8d6cb3fd5f8216b04e092092fae8
yossuli <s1f102302348@iniad.org>
2025-02-21 21:50:42 +0900

    replace: use react

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,30 @@
-import build from '@hono/vite-build/cloudflare-pages'
-import devServer from '@hono/vite-dev-server'
-import adapter from '@hono/vite-dev-server/cloudflare'
-import { defineConfig } from 'vite'
+import pages from "@hono/vite-cloudflare-pages";
+import devServer from "@hono/vite-dev-server";
+import { defineConfig } from "vite";
 
-export default defineConfig({
-  plugins: [
-    build(),
-    devServer({
-      adapter,
-      entry: 'src/index.tsx'
-    })
-  ]
-})
+export default defineConfig(({ mode }) => {
+  if (mode === "client") {
+    return {
+      build: {
+        rollupOptions: {
+          input: "./src/client.tsx",
+          output: {
+            entryFileNames: "static/client.js",
+          },
+        },
+      },
+    };
+  } else {
+    return {
+      ssr: {
+        external: ["react", "react-dom"],
+      },
+      plugins: [
+        pages(),
+        devServer({
+          entry: "src/index.tsx",
+        }),
+      ],
+    };
+  }
+});
