9d2295e33b41007efb8f50a3ca1ed6890c557178
yossuli <s1f102302348@iniad.org>
2025-02-25 04:59:36 +0900

    type Knead Knead 3

--- app/routes/api/test/auth.ts
+++ app/routes/api/test/auth.ts
@@ -1,50 +1,7 @@
 import { createRoute } from "honox/factory";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
-import { Hono } from "hono";
-import { hc } from "hono/client";
-import {
-  BlankEnv,
-  BlankInput,
-  BlankSchema,
-  ExtractInput,
-  H,
-  HandlerResponse,
-  ToSchema,
-} from "hono/types";
-import { z } from "zod";
-import { zValidator } from "@hono/zod-validator";
-const schema = z
-  .object({
-    name: z.string(),
-  })
-  .optional();
 
 export const GET = createRoute(clerkMiddleware(), (c) => {
   const auth = getAuth(c);
   return c.json({ auth });
 });
-
-const app = new Hono();
-
-const route = app.get("/", clerkMiddleware(), (c) => {
-  const auth = getAuth(c);
-  return c.json({ auth });
-});
-
-type GETType = typeof GET;
-
-type App = typeof route;
-
-type PickIN<T extends any[]> =
-  T["0"] extends H<any, any, infer R, any> ? R : never;
-type PickOUT<T extends any[]> =
-  T["0"] extends H<any, any, any, infer R> ? R : never;
-
-type GETS = ToSchema<
-  "GET",
-  "/api/test/auth",
-  ExtractInput<PickIN<GETType>>,
-  PickOUT<GETType>
->;
-
-const client = hc<App & Hono<BlankEnv, GETS, "/">>("");
