b567abf5b31e8aef819029ca1d71703007d3a796
yossuli <s1f102302348@iniad.org>
2025-03-06 21:19:19 +0900

    update: add compact header mode

--- app/tanstackRoutes/__root.tsx
+++ app/tanstackRoutes/__root.tsx
@@ -21,54 +21,55 @@ function RootComponent() {
 
   return (
     <>
+      {compactMode ? (
+        <div style={{ position: "fixed", margin: "1rem", right: 0, top: 0 }}>
+          <TaberseLogo onClick={() => setCompactMode(false)} />
+        </div>
+      ) : (
+        <header
+          style={{
+            position: "sticky",
+            top: 0,
+            backgroundColor: "var(--bg)",
+            padding: "0.67rem",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            borderBottom: "1px solid var(--border)",
+          }}
+        >
+          <h1
+            style={{
+              display: "flex",
+              flexGrow: 1,
+            }}
+          >
+            <TaberseLogo onClick={() => setCompactMode(true)} />
+            <div style={{ width: "1rem" }} />
+            Taberse
+          </h1>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <UserButton />
+            <h5
+              style={{
+                marginLeft: "1rem",
+              }}
+            >
+              {user?.user?.username}
+            </h5>
+            <SignedOut>ログインしていません</SignedOut>
+          </div>
+        </header>
+      )}
       <SignedOut>
         <Outlet />
       </SignedOut>
       <SignedIn>
-        {compactMode ? (
-          <div style={{ position: "fixed", margin: "1rem", right: 0, top: 0 }}>
-            <TaberseLogo onClick={() => setCompactMode(false)} />
-          </div>
-        ) : (
-          <header
-            style={{
-              position: "sticky",
-              top: 0,
-              backgroundColor: "var(--bg)",
-              padding: "0.67rem",
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-between",
-              borderBottom: "1px solid var(--border)",
-            }}
-          >
-            <h1
-              style={{
-                display: "flex",
-                flexGrow: 1,
-              }}
-            >
-              <TaberseLogo onClick={() => setCompactMode(true)} />
-              <div style={{ width: "1rem" }} />
-              Taberse
-            </h1>
-            <div
-              style={{
-                display: "flex",
-                alignItems: "center",
-              }}
-            >
-              <UserButton />
-              <h5
-                style={{
-                  marginLeft: "1rem",
-                }}
-              >
-                {user?.user?.username}
-              </h5>
-            </div>
-          </header>
-        )}
         <Outlet />
       </SignedIn>
     </>
