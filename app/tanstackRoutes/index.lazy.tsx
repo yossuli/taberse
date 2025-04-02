d03e7d4794571232978c29b88c3de110f9a7ac3a
yossuli <s1f102302348@iniad.org>
2025-04-03 00:00:18 +0900

    update: mainpage

--- app/tanstackRoutes/index.lazy.tsx
+++ app/tanstackRoutes/index.lazy.tsx
@@ -1,4 +1,8 @@
-import { Link, createLazyRoute } from "@tanstack/react-router";
+import type { JsonValue } from "@prisma/client/runtime/library";
+import { css, cx } from "@ss/css";
+import { Br } from "@ss/jsx";
+import { flex } from "@ss/patterns";
+import { createLazyRoute } from "@tanstack/react-router";
 import { hc } from "hono/client";
 import { useEffect, useState } from "react";
 import type { Routes } from "../.hc.type";
@@ -9,222 +13,136 @@ export const Route = createLazyRoute("/")({
   component: () => {
     return (
       <>
-        <h1>Hello, Hono with React!</h1>
-        <h2>Example of stream</h2>
-        <Stream />
-        <h2>Example of useState()</h2>
-        <Counter />
-        <h2>Example of API fetch()</h2>
-        <ClockButton />
-        <h2>Example of API fetch() with param</h2>
-        <ReqWithParamButton />
-        <h2>Example of API fetch() with body</h2>
-        <ReqWithBodyButton />
-        <Link to="/form">
-          <button type="button">Jump to form</button>
-        </Link>
-        <form action="api/test/game" method="post">
-          <input type="text" name="title" />
-          <input type="text" name="passPhrase" />
-          <button type="submit">Submit</button>
+        <h1 className={css({ lineHeight: "1.4 !important" })}>
+          ようこそ <mark>Taberse</mark> へ!!
+        </h1>
+        <Notice />
+        <h2>ルームを作成する</h2>
+        <form action="/" method="post">
+          <label htmlFor="passphrase">
+            <h4>あいことばを入力してください</h4>
+          </label>
+          <input type="text" id="passphrase" />
+          <label htmlFor="rules">
+            <h4>ルールを選択してください</h4>
+          </label>
+          <Rules />
+          <button type="submit">作成</button>
         </form>
-        <h2>Example of API fetch() to DB</h2>
-        <FetchToDB />
+        <h2>ルールを作成する</h2>
+        <form action="/" method="post">
+          <label htmlFor="title">
+            <h4>ルールのタイトルを入力してください</h4>
+          </label>
+          <input type="text" id="title" />
+          <h4>ルールの詳細を入力してください</h4>
+          <button type="submit">作成</button>
+        </form>
+        <h2>ルームを検索する</h2>
+        <form action="/" method="post">
+          <label htmlFor="passphrase">
+            <h4>あいことばを入力してください</h4>
+          </label>
+          <div>
+            <input type="text" id="passphrase" />
+            <button type="submit">入室</button>
+          </div>
+        </form>
       </>
     );
   },
 });
 
-function Stream() {
-  const [isStreaming, setIsStreaming] = useState(false);
-  const [response, setResponse] = useState<string | null>(null);
+const Notice = () => {
+  return (
+    <p
+      className={cx(
+        flex({
+          display: "flex",
+          flexWrap: "wrap",
+          marginTop: "0!",
+          "& > div": {
+            w: "100%",
+          },
+        }),
+        "notice",
+      )}
+    >
+      <span>ここでは、</span>
+      <span>現実世界で</span>
+      <span>ボードゲームを</span>
+      <span>遊ぶ</span>
+      <span>ように</span>
+      <span>自由に</span>
+      <span>ルールを</span>
+      <span>決めて</span>
+      <span>遊ぶことが</span>
+      <span>できます。</span>
+      <Br />
+      <span>この</span> <span>Taberse</span> <span>は、</span>
+      <span>ルールの</span>
+      <span>検証を</span>
+      <span>行いません。</span>
+      <Br />
+      <span>プレイヤーの</span>
+      <span>皆さんは</span>
+      <span>外部ツールで</span>
+      <span>コミュニケーションを</span>
+      <span>取りながら</span>
+      <span>ルールを</span>
+      <span>守って</span>
+      <span>遊んで</span>
+      <span>ください。</span>
+      <Br />
+      {/* <button
+        type="button"
+        className={float({
+          position: "absolute",
+          right: "0",
+          top: "0",
+          w: "8",
+          aspectRatio: "1/1",
+          "&::after, &::before": {
+            position: "absolute",
+            content: "''",
+            w: "8",
+            h: "1",
+            bg: "var(--bg)",
+            zIndex: -1,
+            transform: "translate(-50%,-50%) rotate(45deg)",
+            borderRadius: "2",
+          },
+          "&::before": {
+            transform: "translate(-50%,-50%) rotate(-45deg)",
+          },
+        })}
+      /> */}
+    </p>
+  );
+};
 
+const Rules = () => {
+  const [rules, setRules] = useState<
+    {
+      title: string;
+      rule: JsonValue;
+    }[]
+  >([]);
   useEffect(() => {
-    if (!isStreaming) {
-      return;
-    }
-
-    const abortController = new AbortController(); // AbortControllerを作成
-    const stream = client.api.test.stream.$get();
-
-    stream.then((response) => {
-      const reader = response.body?.getReader();
-      if (!reader) {
-        return;
-      }
-
-      const readStream = () => {
-        // biome-ignore lint/complexity/noExcessiveCognitiveComplexity: <explanation>
-        reader.read().then(({ done, value }) => {
-          if (done || abortController.signal.aborted) {
-            console.log("Stream complete");
-            return;
-          }
-          const text = new TextDecoder().decode(value);
-          setResponse(text);
-          readStream(); // 再帰的に呼び出し
-        });
-      };
-
-      readStream();
-    });
-
-    return () => {
-      abortController.abort(); // クリーンアップ時にストリームを中断
+    const fetchRules = async () => {
+      const res = await client.api.test.game.$get();
+      const data = await res.json();
+      setRules(data.games);
     };
-  }, [isStreaming]);
-
+    fetchRules();
+  }, []);
   return (
-    <>
-      <button
-        type="button"
-        onClick={() => {
-          console.log("clicked");
-          setIsStreaming((c) => !c);
-        }}
-      >
-        Stream
-      </button>
-      <button
-        type="button"
-        onClick={() => {
-          client.api.test.stream.$put();
-        }}
-      >
-        Put
-      </button>
-      {isStreaming ? "Streaming" : "Not streaming"}
-      {response && <pre>{response}</pre>}
-    </>
-  );
-}
-
-function Counter() {
-  const [count, setCount] = useState(0);
-  return (
-    <button type="button" onClick={() => setCount(count + 1)}>
-      You clicked me {count} times
-    </button>
-  );
-}
-
-const ClockButton = () => {
-  const [response, setResponse] = useState<string | null>(null);
-
-  const handleClick = async () => {
-    const response = await client.api.test.$get();
-    const data = await response.json();
-    const headers = Array.from(response.headers.entries()).reduce(
-      (acc, [key, value]) => ({ ...acc, [key]: value }),
-      {},
-    );
-    const fullResponse = {
-      url: response.url,
-      status: response.status,
-      headers,
-      body: data,
-    };
-    setResponse(JSON.stringify(fullResponse, null, 2));
-  };
-
-  return (
-    <>
-      <button type="button" onClick={handleClick}>
-        Get Server Time
-      </button>
-      {response && <pre>{response}</pre>}
-    </>
-  );
-};
-
-const ReqWithParamButton = () => {
-  const [response, setResponse] = useState<string | null>(null);
-  const [id, setId] = useState(0);
-  const handleClick = async () => {
-    const response = await client.api.test.id[":id"].$get({
-      param: { id: String(id) },
-    });
-    const data = await response.json();
-    const headers = Array.from(response.headers.entries()).reduce(
-      (acc, [key, value]) => ({ ...acc, [key]: value }),
-      {},
-    );
-    const fullResponse = {
-      url: response.url,
-      status: response.status,
-      headers,
-      body: data,
-    };
-    setResponse(JSON.stringify(fullResponse, null, 2));
-  };
-
-  return (
-    <>
-      <input type="number" onChange={(e) => setId(+e.target.value)} />
-      <button type="button" onClick={handleClick}>
-        Get param
-      </button>
-      {response && <pre>{response}</pre>}
-    </>
-  );
-};
-
-const ReqWithBodyButton = () => {
-  const [response, setResponse] = useState<string | null>(null);
-  const [name, setName] = useState("");
-  const handleClick = async () => {
-    const response = await client.api.test.$post({
-      json: { name },
-    });
-    const data = await response.json();
-    const headers = Array.from(response.headers.entries()).reduce(
-      (acc, [key, value]) => ({ ...acc, [key]: value }),
-      {},
-    );
-    const fullResponse = {
-      url: response.url,
-      status: response.status,
-      headers,
-      body: data,
-    };
-    setResponse(JSON.stringify(fullResponse, null, 2));
-  };
-
-  return (
-    <>
-      <input type="text" onChange={(e) => setName(e.target.value)} />
-      <button type="button" onClick={handleClick}>
-        Post name
-      </button>
-      {response && <pre>{response}</pre>}
-    </>
-  );
-};
-
-const FetchToDB = () => {
-  const [response, setResponse] = useState<string | null>(null);
-  const handleClick = async () => {
-    const response = await client.api.test.game.$get();
-    const data = await response.json();
-    const headers = Array.from(response.headers.entries()).reduce(
-      (acc, [key, value]) => ({ ...acc, [key]: value }),
-      {},
-    );
-    const fullResponse = {
-      url: response.url,
-      status: response.status,
-      headers,
-      body: data,
-    };
-    setResponse(JSON.stringify(fullResponse, null, 2));
-  };
-
-  return (
-    <>
-      <button type="button" onClick={handleClick}>
-        Get games
-      </button>
-      {response && <pre>{response}</pre>}
-    </>
+    <select name="rules" id="rules">
+      {rules.map((rule) => (
+        <option key={rule.title} value={rule.title}>
+          {rule.title}
+        </option>
+      ))}
+    </select>
   );
 };
