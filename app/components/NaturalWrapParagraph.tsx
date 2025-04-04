227ad89b860f6136a67142f096e0937bb513eff4
yossuli <s1f102302348@iniad.org>
2025-04-04 23:59:46 +0900

    add: NaturalWrapParagraph

---
+++ app/components/NaturalWrapParagraph.tsx
@@ -0,0 +1,38 @@
+import { cx } from "@ss/css";
+import { Br } from "@ss/jsx";
+import { flex } from "@ss/patterns";
+import type { ReactNode } from "react";
+
+type Props = {
+  paragraph: ReactNode[][];
+  className?: string;
+};
+
+export const NaturalWrapParagraph = ({ paragraph, className }: Props) => {
+  return (
+    <p
+      className={cx(
+        flex({
+          display: "flex",
+          flexWrap: "wrap",
+          "& > div": {
+            w: "100%",
+          },
+          "& > span": {
+            minWidth: "1",
+          },
+        }),
+        className,
+      )}
+    >
+      {paragraph.map((sentence, j) => (
+        <>
+          {sentence.map((text, i) => (
+            <span key={`${j}-${i}`}>{text}</span>
+          ))}
+          <Br key={j} />
+        </>
+      ))}
+    </p>
+  );
+};
