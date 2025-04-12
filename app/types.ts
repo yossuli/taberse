deba6535aeffb30d1a4920bddc1e3453b3ec3e7c
yossuli <s1f102302348@iniad.org>
2025-04-13 03:55:31 +0900

    fix: props type

--- app/types.ts
+++ app/types.ts
@@ -1,19 +1,28 @@
 import type { RuleSchema } from "app/zodSchemas";
 import type {
-  Control,
   FieldErrors,
-  UseFormRegisterReturn,
+  UseFormRegister,
   UseFormTrigger,
 } from "react-hook-form";
 import type { z } from "zod";
 
 export type RuleType = z.infer<typeof RuleSchema>;
 
-export type RuleMakeFormChildrenOmitRegisterProps = {
+export type RuleMakeFormChildrenProps = {
+  // watch: UseFormWatch<RuleType>;
+  // getValues: UseFormGetValues<RuleType>;
+  // getFieldState: UseFormGetFieldState<RuleType>;
+  // setError: UseFormSetError<RuleType>;
+  // clearErrors: UseFormClearErrors<RuleType>;
+  // setValue: UseFormSetValue<RuleType>;
+  trigger: UseFormTrigger<RuleType>;
+  // formState: FormState<RuleType>;
+  // resetField: UseFormResetField<RuleType>;
+  // reset: UseFormReset<RuleType>;
+  // unregister: UseFormUnregister<RuleType>;
+  // control: Control<RuleType>;
+  register: UseFormRegister<RuleType>;
+  // setFocus: UseFormSetFocus<RuleType>;
+  // subscribe: UseFromSubscribe<RuleType>;
   errors: FieldErrors<RuleType>;
-  control?: Control<RuleType>;
-  trigger?: UseFormTrigger<RuleType>;
 };
-
-export type RuleMakeFormChildrenProps = RuleMakeFormChildrenOmitRegisterProps &
-  Record<`register${string}`, UseFormRegisterReturn<string>>;
