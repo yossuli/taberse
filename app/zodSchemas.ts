42fea8afad762ba7132de9cdde85725bee44dc41
yossuli <s1f102302348@iniad.org>
2025-04-23 00:57:03 +0900

    update: zodSchema

--- app/zodSchemas.ts
+++ app/zodSchemas.ts
@@ -97,33 +97,69 @@ export const RuleSchema = z.object({
     ]),
   ),
   fieldArea: z.array(
-    z.object({
-      name: z.string(),
-      description: z.string(),
-      roleFor: z.union([
-        z.literal("main"),
-        z.intersection(z.string(), z.object({})),
-      ]),
-      fieldSize: z.object({
-        width: z.number().positive(),
-        height: z.number().positive(),
-      }),
-      field: z.array(
-        z.object({
-          name: z.string(),
-          description: z.string(),
-          color: z.string(),
-          area: z.object({
-            l: z.number().int().nonnegative(),
-            t: z.number().int().nonnegative(),
-            r: z.number().int().nonnegative(),
-            b: z.number().int().nonnegative(),
-          }),
-          operableRoles: z.array(z.object({ name: z.string() })),
-          visibleRoles: z.array(z.object({ name: z.string() })),
+    z
+      .object({
+        name: z.string(),
+        description: z.string(),
+        roleFor: z.union([
+          z.literal("main"),
+          z.intersection(z.string(), z.object({})),
+        ]),
+        fieldSize: z.object({
+          width: z.number().positive(),
+          height: z.number().positive(),
         }),
-      ),
-    }),
+        field: z
+          .array(
+            z.object({
+              name: z.string(),
+              description: z.string(),
+              color: z.string().regex(/^(#([0-9a-f]{3}|[0-9a-f]{6})|)$/i, {
+                message: "Invalid color format",
+              }),
+              area: z.object({
+                l: z.number().int().nonnegative(),
+                t: z.number().int().nonnegative(),
+                r: z.number().int().nonnegative(),
+                b: z.number().int().nonnegative(),
+              }),
+              operableRoles: z.array(z.object({ name: z.string() })),
+              visibleRoles: z.array(z.object({ name: z.string() })),
+            }),
+          )
+          .superRefine((field, ctx) => {
+            field.forEach(({ name }, index) => {
+              if (
+                field.filter((_, i) => i !== index).find((f) => f.name === name)
+              ) {
+                ctx.addIssue({
+                  message: `Field "${name}" is duplicated`,
+                  code: z.ZodIssueCode.custom,
+                  path: [index, "name"],
+                });
+              }
+            });
+          }),
+      })
+      .superRefine((fieldArea, ctx) => {
+        fieldArea.field.forEach(({ area, name }, index) => {
+          if (
+            area.r > fieldArea.fieldSize.width ||
+            area.b > fieldArea.fieldSize.height
+          ) {
+            ctx.addIssue({
+              message: `Field "${name}" is out of fieldSize`,
+              code: z.ZodIssueCode.custom,
+              path: [index, "area"],
+            });
+            ctx.addIssue({
+              message: `This fieldArea must have a ${fieldArea.fieldSize.width} x ${fieldArea.fieldSize.height} size`,
+              code: z.ZodIssueCode.custom,
+              path: [index, "fieldSize"],
+            });
+          }
+        });
+      }),
   ),
   dice: z
     .array(
