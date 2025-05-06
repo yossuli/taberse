57ecdea74ec5684f339145f9eefd2ab2d52034d7
yossuli <s1f102302348@iniad.org>
2025-05-07 02:41:31 +0900

    add: test to objArr2StrArr

--- app/utils/objArr2StrArr.ts
+++ app/utils/objArr2StrArr.ts
@@ -2,3 +2,24 @@ export const objArr2StrArr = <T extends Record<string, any>, U extends keyof T>(
   objArray: T[] | undefined,
   key: U,
 ) => objArray?.map((obj) => obj[key]);
+
+if (import.meta.vitest) {
+  const { it, expect, expectTypeOf } = import.meta.vitest;
+  it("objArr2StrArr normal test", () => {
+    const objArray = [{ name: "a" }, { name: "b" }, { name: "c" }];
+    const result = objArr2StrArr(objArray, "name");
+    expect(result).toEqual(["a", "b", "c"]);
+  });
+
+  it("objArr2StrArr empty array test", () => {
+    const empty: { id: number; name: string }[] = [];
+    const result = objArr2StrArr(empty, "name");
+    expect(result).toEqual([]);
+  });
+
+  it("objArr2StrArr undefined test", () => {
+    const optArray = undefined;
+    const result = objArr2StrArr(optArray, "name");
+    expect(result).toEqual(undefined);
+  });
+}
